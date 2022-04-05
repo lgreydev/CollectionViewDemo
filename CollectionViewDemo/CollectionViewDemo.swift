cf6e0133ac248916b403af8884dde91e31a5b922
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-04-05 11:28:02 +0300

    Add data source collection view

--- CollectionViewDemo/CollectionViewDemo.swift
+++ CollectionViewDemo/CollectionViewDemo.swift
@@ -8,13 +8,42 @@
 import UIKit
 import AloeStackView
 
-class CollectionViewDemo: AloeStackViewController {
+class CollectionViewDemo: UIViewController {
 
+
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .blue
+
+        setUpCollectionView()
     }
+
+
+    private func setUpCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .lightGray
+        view.addSubview(collectionView)
+
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellID")
+    }
+}
+
+extension CollectionViewDemo: UICollectionViewDelegate, UICollectionViewDataSource {
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        <#code#>
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+
 
 }
